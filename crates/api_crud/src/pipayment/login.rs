a12f1cded5b15a834f685780a4ca5a574f1d4e5e
Pi Connect Developers <piconnectdev@gmail.com>
2022-12-07 11:01:10 +0700

    Fix PiLogin

--- crates/api_crud/src/pipayment/login.rs
+++ crates/api_crud/src/pipayment/login.rs
@@ -58,19 +58,12 @@ impl PerformCrud for PiLogin {
 
     let data: &PiLogin = &self;
     let settings = SETTINGS.to_owned();
-    // Make sure site has open registration
-    // if let Ok(site) = blocking(context.pool(), move |conn| Site::read_local_site(conn)).await? {
-    //   if !site.open_registration {
-    //     return Err(LemmyError::from_message("registration_closed"));
-    //   }
-    // }
     let site_view = SiteView::read_local(context.pool()).await?;
     let local_site = site_view.local_site;
 
     if !local_site.open_registration {
-      return Err(LemmyError::from_message("registration_closed"));
+      //return Err(LemmyError::from_message("registration_closed"));
     }
-
 
     if !settings.pinetwork.pi_free_login {
       //return Err(LemmyError::from_message("registration_closed"));
@@ -97,7 +90,7 @@ impl PerformCrud for PiLogin {
       Err(_e) => {
         // Pi Server error
         let err_type = format!(
-          "Pi Server Error: User not found: {}, error: {}",
+          "Pi Network Server Error: User not found: {}, error: {}",
           &data.ea.account,
           _e.to_string()
         );
@@ -105,48 +98,33 @@ impl PerformCrud for PiLogin {
       }
     };
 
-    let mut sha256 = Sha256::new();
-    sha256.update(settings.pi_seed());
-    sha256.update(_pi_username.clone());
-    let _pi_alias: String = format!("{:X}", sha256.finalize());
-    let _pi_alias2 = _pi_alias.clone();
-    let _pi_alias3 = _pi_alias.clone();
-    //let _pi_alias = data.pi_username.to_owned();
-
+    let _pi_alias = _pi_username.clone();
     let mut username = _pi_username.clone();
-    let mut _new_user: Sensitive<String> = Sensitive::from(username.clone());
+
+    let mut _new_user: Sensitive<String> = Sensitive::from(_pi_alias.clone());
     let mut _new_password = Sensitive::from("".to_string()); //info.password.to_owned();
 
     let person_id: PersonId;
     let mut pi_exist = false;
-    let mut result = true;
-    let mut create_new = false;
+    let mut _change_passwd = false;
 
     match &data.info {
       Some(info) => {
-        create_new = true;
+        _change_passwd = true;
         _new_user = info.username_or_email.clone();
         _new_password = info.password.clone();
       }
       None => {}
     }
 
-    // Check if there are admins. False if admins exist
-    // let no_admins = blocking(context.pool(), move |conn| {
-    //   PersonViewSafe::admins(conn).map(|a| a.is_empty())
-    // })
-    // .await??;
-    let no_admins = PersonViewSafe::admins(context.pool()).await.map(|a| a.is_empty());
-
-    if create_new {
+    if _change_passwd {
       password_length_check(&_new_password)?;
       // // Make sure passwords match
       // if info.password != info.password_verify {
       //   return Err(LemmyError::from_message("passwords_dont_match"));
       // }
 
-      // // If its not the admin, check the captcha
-      // if !no_admins && Settings::get().captcha.enabled {
+      // if local_site.site_setup && local_site.captcha_enabled {
       //   let check = context
       //     .chat_server()
       //     .send(CheckCaptcha {
@@ -163,29 +141,29 @@ impl PerformCrud for PiLogin {
       //     })
       //     .await?;
       //   if !check {
-      //     return Err(LemmyError::from_message("captcha_incorrect"));
+      //     return Err(LemmyError::from_message("captcha_incorrect").into());
       //   }
       // }
     }
 
     // Find user exist ?
-    let pi_person = match Person::find_by_extra_name(context.pool(), &_pi_alias).await
+    let person = match Person::find_by_extra_name(context.pool(), &_pi_alias).await
     {
       Ok(c) => Some(c),
       Err(_e) => None,
     };
 
     let mut external_id = None;
-    match pi_person {
-      Some(pi) => {
+    match person {
+      Some(p) => {
         pi_exist = true;
-        person_id = pi.id;
-        username = pi.name.clone();
-        external_id = pi.external_id;
+        person_id = p.id;
+        username = p.name.clone();
+        external_id = p.external_id;
       }
       None => {
-        if !create_new {
-          let err_type = format!("Hi {}, you must register before login.", &username);
+        if !_change_passwd {
+          let err_type = format!("Hi {}, you must register before login.", &username.clone());
           println!("{} {}", _pi_uid.unwrap(), err_type);
           return Err(LemmyError::from_message(&err_type));
         }
@@ -194,8 +172,7 @@ impl PerformCrud for PiLogin {
 
     if pi_exist {
       let local_user_id;
-      let username2 = username.clone();
-      let _local_user = match LocalUserView::read_from_name(context.pool(), &username2.clone()).await
+      let _local_user = match LocalUserView::read_from_name(context.pool(), &username.clone()).await
       {
         Ok(lcu) => lcu.local_user,
         Err(_e) => {
@@ -208,18 +185,12 @@ impl PerformCrud for PiLogin {
           println!("{} {}", _pi_uid.unwrap().clone(), err_type);
           return Err(LemmyError::from_error_message(_e, &err_type));
 
-          //  return Ok(PiRegisterResponse {
-          //   success: false,
-          //   jwt: format!(""),
-          //   extra: Some(format!("{}",err_type)),
-          //   });
         }
       };
 
       local_user_id = _local_user.id.clone();
 
-      //  let password_hash = hash(_new_password.clone(), DEFAULT_COST).expect("Couldn't hash password");
-      if create_new {
+      if _change_passwd {
         let updated_local_user = match LocalUser::update_password(context.pool(), local_user_id.clone(), &_new_password).await
         {
           Ok(lcu) => lcu,
@@ -233,21 +204,6 @@ impl PerformCrud for PiLogin {
           }
         };
       }
-      // let _pi_uid_search = _pi_uid.clone();
-      // let  _payment = match blocking(context.pool(), move |conn| {
-      //   PiPayment::find_by_pi_uid(&context.pool(), &_pi_uid_search)
-      // })
-      // .await?
-      // {
-      //   Ok(c) => {
-      //     Some(c)
-      //   }
-      //   Err(_e) => {
-      //     let err_type = format!("Invalid pi user id {}", &_new_user.clone());
-      //     println!("{} {}", _pi_uid.clone(), err_type);
-      //     return Err(LemmyError::from_message(&err_type).into());
-      //   },
-      // };
 
       return Ok(LoginResponse {
         jwt: Some(
@@ -262,13 +218,13 @@ impl PerformCrud for PiLogin {
         verify_email_sent: false,
         registration_created: false,
       });
-    } // Pi exist
+    } // User exist
 
     // We have to create both a person, and local_user
     //if !create_new 
     {
       let err_type = format!(
-        "Auto create new account for Pioneers is disabled {} {}",
+        "Auto create new account is disabled {} {}",
         &_new_user.to_string().clone(),
         &_pi_uid.unwrap().clone()
       );
